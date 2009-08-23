3ce70041819d8249ce511d307b3e367155188646
drewp <drewp@bigasterisk.com>
2009-08-23 02:22:01 -0700

    start
    Ignore-this: 1aa4899d7ab2899efa3b5f6f20ea03e0
    
    darcs-hash:20090823092201-312f9-6d1ef0be4668b8cfce329387f206459594cbd389.gz

---
+++ parports.py
@@ -0,0 +1,109 @@
+from fcntl import ioctl
+import os, struct
+
+IOPWRITE = 2
+class SolarisParport(object):
+    """alternate implementation of an ieee1284.Port
+
+    where you might use this elsewhere:
+        import ieee1284
+        ports = ieee1284.find_ports()
+        port = ports['0x3bc']
+        port.open()
+        
+    ..this version seems to work on solaris x86:
+        port = SolarisParport(port=0x3bc)
+        port.open()
+    
+    """
+    def __init__(self, file='/devices/pseudo/iop@0:iop', port=0x3bc):
+        self.file, self.port = file, port
+
+    def open(self):
+        self.fd = os.open(self.file, os.O_RDWR)
+        return "capabilities return value not implemented"
+
+    def write_data(self, value):
+        ioctl(self.fd, IOPWRITE, self._iopbuf_struct(self.port, value))
+        
+    def _iopbuf_struct(self, port, port_value):
+        return struct.pack('IB', port, port_value)
+
+# see also http://www.captain.at/programming/solaris/ for the structs,
+# IOP constants
+
+
+# not working yet
+class Usbprn(object):
+    """a solaris parallel port on usb"""
+    def __init__(self, file="/dev/printers/0"):
+        self.file = file
+
+    def open(self):
+        print "opening - sun hangs here"
+        self.fd = os.open(self.file, os.O_RDWR)
+        print "opened"
+        return "capabilities return value not implemented"
+
+    def write_data(self, value):
+        print "writing"
+        os.write(self.fd, chr(value))
+        print "wrote"
+
+class LinuxUsbParallel(object):
+    def __init__(self, file="/dev/usblp1"):
+        self.file = file
+
+    def open(self):
+         self.fd = os.open(self.file, os.O_RDWR)
+
+    def write_data(self, value):
+        print "writing"
+        os.write(self.fd, chr(value))
+        print "wrote"
+
+class LinuxParallel(object):
+    def __init__(self, port=0):
+        self.portNum = port
+
+    def open(self):
+        import parallel
+        try:
+            self.port = parallel.Parallel(port=self.portNum)
+        except IOError:
+            print "may need 'rmmod lp'"
+            raise
+        self.device = self.port.device
+
+    def write_data(self, value):
+        self.port.setData(value)
+
+    def getInBusy(self):
+        """matching the ieee1284.Port api would be awkward here, so
+        i'll just use a bit of the parallel.Parallel one too"""
+        return self.port.getInBusy()
+        
+
+class NullPort(object):
+    device = 'null'
+    def write_data(self, byte):
+        pass
+    def getInBusy(self):
+        return False
+    getInAcknowledge = getInBusy
+    getInPaperOut = getInBusy
+    getInSelected = getInBusy
+    getInError = getInBusy
+
+if __name__ == '__main__':
+    p = LinuxUsbParallel()
+    p.open()
+    import time
+    while 1:
+        print "hi"
+        p.write_data(255)
+        time.sleep(1)
+        print "lo"
+        p.write_data(0)
+        time.sleep(1)
+        
